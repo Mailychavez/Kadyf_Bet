c043ffa51cb0d2d670de38528abf8f7c90599da3
Maily Chavez <mailychavez18@gmail.com>
2024-07-29 19:19:27 -0600

    nuevo

---
+++ java.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuItems = document.getElementById("menuItems");
+    menuItems.classList.toggle("show");
+  
+    var menuToggle = document.querySelector(".menu-toggle");
+    menuToggle.classList.toggle("active");
+  }
+  
